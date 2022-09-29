d72f23d72c650fb1e919e12554666a8c4a1c897c
SergBily <sergBily.dev@gmail.com>
2022-09-29 12:36:21 +0300

    refactor: add angular material

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import YoutubeModule from './youtube/youtube.module';
@@ -12,6 +13,7 @@ import YoutubeModule from './youtube/youtube.module';
     BrowserModule,
     AppRoutingModule,
     YoutubeModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
