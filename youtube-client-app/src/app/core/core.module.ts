bdf4a7e6e4aac4834033a4f3be81dd104963607f
SergBily <sergBily.dev@gmail.com>
2022-10-18 20:38:21 +0300

    feat: add default config

--- youtube-client-app/src/app/core/core.module.ts
+++ youtube-client-app/src/app/core/core.module.ts
@@ -5,10 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import HeaderComponent from './components/header/header.component';
 import InputSearchComponent from './components/input-search/input-search.component';
 import SortComponent from './components/sort/sort.component';
 import PageNotFoundComponent from './pages/page-not-found/page-not-found.component';
+import { APP_CONFIG, DEFAULT_CONFIG } from '../shared/config/app.config';
 
 @NgModule({
   declarations: [
@@ -23,10 +25,12 @@ import PageNotFoundComponent from './pages/page-not-found/page-not-found.compone
     MatIconModule,
     FormsModule,
     MatButtonModule,
+    HttpClientModule,
     IconSpriteModule.forRoot({ path: 'assets/svg/sprite-main.svg' }),
   ],
   exports: [
     HeaderComponent,
   ],
+  providers: [{ provide: APP_CONFIG, useValue: DEFAULT_CONFIG }],
 })
 export default class CoreModule { }
